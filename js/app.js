071c609a146008a2633ffdc181dd07462f30f628
EmmAlva <clioemma_28@hotmail.com>
2017-07-08 22:25:30 -0500

    portada

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
  $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-
- 
-        
+    $('.modal').modal();  
 
 
 
@@ -12,45 +9,51 @@
 var cv4 = document.getElementById("myCanvas");
 var ctx = cv4.getContext("2d");
 
- ctx.beginPath();
- 
-                    ctx.moveTo(450, 19);
- 
-                    ctx.lineTo(450,31);
- 
-                    ctx.lineTo(470,31);
- 
-                    ctx.lineTo(470,220);   
-                    
-                    ctx.lineTo(450,220);
-                    
-                    ctx.lineTo(450,232);
- 
-                    ctx.lineTo(482,232);                    
-              
-                    ctx.lineTo(482,19);
+    ctx.fillStyle = "rgb( 255, 255, 255)";    
+    
+//    ctx.lineWidth = 2;
+//ctx.strokeStyle = "red"; // Definimos el color para pintar el borde
+//ctx.stroke(); //dibuja el borde
 
+    ctx.beginPath();
+ 
+                    ctx.moveTo(10,5); //punto inicial
+ 
+                    ctx.lineTo(10,145);
+ 
+                    ctx.lineTo(60,145);
+ 
+                    ctx.lineTo(60,137);   
+                    
+                    ctx.lineTo(25,137);
+                    
+                    ctx.lineTo(25,13);
+
+                    ctx.lineTo(60,13);
+ 
+                    ctx.lineTo(60,5);                    
+              
                     ctx.closePath();
  
                     ctx.fill();
 
 ctx.beginPath();
  
-                    ctx.moveTo(20,19);
+                    ctx.moveTo(240,5);
  
-                    ctx.lineTo(20,200);
+                    ctx.lineTo(240,13);
  
-                    ctx.lineTo(60,200); //ancho-inf
+                    ctx.lineTo(275,13); //ancho-inf
  
-                    ctx.lineTo(60,180); //ancho-inf
+                    ctx.lineTo(275,137); //ancho-inf
  
-                    ctx.lineTo(40,180);//ancho
+                    ctx.lineTo(240,137);//ancho
  
-                    ctx.lineTo(40,39); //ancho
+                    ctx.lineTo(240,145); //ancho
                     
-                    ctx.lineTo(60,39); //ancho-sup
+                    ctx.lineTo(290,145); //ancho-sup
                      
-                    ctx.lineTo(60,19); //ancho-sup
+                    ctx.lineTo(290,5); //ancho-sup
  
                     ctx.closePath();
  
